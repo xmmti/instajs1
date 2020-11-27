a33a7613b76f17437f1d4193ffd1eb4a1c1972d3
X7md <54203033+X7md@users.noreply.github.com>
2020-11-27 13:39:53 +0300

    add 400 handling...

--- main.js
+++ main.js
@@ -28,4 +28,15 @@ inquirer
   function ig_login(username, password){
     let data = `username=${username}&enc_password=#PWD_INSTAGRAM_BROWSER:0:1:${password}&queryParams={}&optIntoOneTap=false`;const url = "https://instagram.com/accounts/login/ajax/";
     const options = {headers: {'User-Agent': 'Mozilla/5.0 (Linux; Android 10; SM-A505F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.110 Mobile Safari/537.36 ', 'X-Csrftoken': 'missing', 'Accept-Language': 'en-US,en;q=0.9,ar-SA;q=0.8,ar;q=0.7', 'X-Instagram-Ajax': '1', 'X-Requested-With': 'XMLHttpRequest', 'Content-Type': 'application/x-www-form-urlencoded'}};
-    axios.post(url, data, options).then((re)=>{if(re.data.authenticated === true){re.headers["set-cookie"].forEach( (cookie_) => {var regex = /^(\w+|\w+_\w+)=(.*?)\;/ig; console.log(regex.exec(cookie_)[0])})}})}
+    axios.post(url, data, options).then((re)=>{if(re.data.authenticated === true){re.headers["set-cookie"].forEach( (cookie_) => {var regex = /^(\w+|\w+_\w+)=(.*?)\;/ig; console.log(regex.exec(cookie_)[0])})}}).catch((error) => {
+      // Error 😨
+      if (error.response) {
+          //console.log(error.response.data);
+          var regex = /(mid)=(.*?)\;/ig;
+          if (error.response.data.message === "checkpoint_required"){
+            console.log("https://i.instagram.com" + error.response.data.checkpoint_url + "?__a=1")
+            console.log(regex.exec(error.response.headers["set-cookie"].toString())[0])
+          }
+          //console.log(error.response.status);
+      } 
+  })}
